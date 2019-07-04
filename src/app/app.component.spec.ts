e1182a3fdc8918e033d7e2fcc0d1abcae41b9bce
Tommi Laakso <laakso.tommi@gmail.com>
2019-07-05 00:27:15 +0300

    Fix attempt
    Fix #3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'AngTest'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('AngTest');
+    expect(app.title).toEqual('Angular, Travis CI & Firebase Hosting');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to AngTest!');
+    expect(compiled.querySelector('h1').textContent).toContain('Angular, Travis CI & Firebase Hosting');
   });
 });
